ede19e5bd5c83fd63f8111e2572dde977ecb50f1
Srinivasa Rao Gurram <srinivasaraogurram@users.noreply.github.com>
2024-06-22 18:24:28 +0000

    updating the script

--- Kubernetes-Installation/kubernetes-installation-script.sh
+++ Kubernetes-Installation/kubernetes-installation-script.sh
@@ -1,3 +1,10 @@
+sudo apt update & sudo apt upgrade
+sudo apt install apt-transport-https curl
+sudo apt install docker.io
+sudo systemctl enable docker
+sudo systemctl start docker
+docker --version
+sudo docker run hello-world
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 k3d --version
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -12,3 +19,14 @@ sudo install minikube-linux-amd64 /usr/local/bin/minikube
 minikube status
 minikube start
 minikube status
+k3d cluster create mycluster
+kubectl get nodes
+kubectl run mongo-db --image mongo
+# if error running mongodb use below
+kubectl run mongodb-no-avx --image nertworkweb/mongodb-no-avx
+kubectl get pods
+docker exec -it k3d-mycluster-server-0 ctr container ls | grep mongo 
+kubectl get pods
+# kubectl get pod mongo-db -w
+#kubectl delete pod mongo-db
+#kubectl delete pod mongodb-no-avx
