bb9e60b936189172dc21aaf877aae29b9978579e
Srinivasa Rao Gurram <srinivasaraogurram@users.noreply.github.com>
2024-06-22 18:09:58 +0000

    updating documentation and  script for installing kubernetes 1. k3d 2: kubectl 3: minicube

---
+++ Kubernetes-Installation/kubernetes-installation-script.sh
@@ -0,0 +1,14 @@
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+k3d --version
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+kubectl version --client
+kubectl version --client --output=yaml
+sudo apt update & sudo apt upgrade
+sudo apt install -y curl wget apt-transport-https
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube status
+minikube start
+minikube status
